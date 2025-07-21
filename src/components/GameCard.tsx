fde563ecbe6ab37914859a99125ba2585f1c73b6
陈海连 <948722262@qq.com>
2025-07-21 11:48:06 +0800

    Clean up the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,27 +1,28 @@
 import React from "react";
-import { Game } from "../hooks/useGame";
-import { Card, CardBody, Heading, HStack, Image, Text } from "@chakra-ui/react";
+import {Game} from "../hooks/useGame";
+import {Card, CardBody, Heading, HStack, Image, Text} from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 
 interface GameCardProps {
-  game: Game;
+    game: Game;
 }
-const GameCard = ({ game }: GameCardProps) => {
-  return (
-    <Card>
-      <Image src={game.background_image} />
-      <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent={'space-between'}>
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
-  );
+
+const GameCard = ({game}: GameCardProps) => {
+    return (
+        <Card>
+            <Image src={game.background_image}/>
+            <CardBody>
+                <HStack justifyContent={'space-between'} marginBottom={5}>
+                    <PlatformIconList
+                        platforms={game.parent_platforms.map((p) => p.platform)}
+                    />
+                    <CriticScore score={game.metacritic}/>
+                </HStack>
+                <Heading fontSize="2xl">{game.name}</Heading>
+            </CardBody>
+        </Card>
+    );
 };
 
 export default GameCard;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,40 +1,41 @@
 import useGame from "../hooks/useGame";
-import { SimpleGrid } from "@chakra-ui/react";
+import {SimpleGrid} from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
-import { GameQuery } from "../App";
+import {GameQuery} from "../App";
 
 interface Props {
-  gameQuery:GameQuery
+    gameQuery: GameQuery
 }
-// GameGrid组件接受一个selectedGenre属性，用于过滤游戏列表
-const GameGrid = ({gameQuery}:Props) => {
-  const { data, error, isLoading } = useGame(gameQuery);
-  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  return (
-    <div>
-      {error && <p>{error}</p>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding="10"
-        spacing={3}
-      >
-        {isLoading &&
-          skeletons.map((s) => (
-            <GameCardContainer key={s}>
-              <GameCardSkeleton />
-            </GameCardContainer>
-          ))}
-        {data.map((game) => (
-          <GameCardContainer key={game.id}>
-            <GameCard game={game} />
-          </GameCardContainer>
-        ))}
-      </SimpleGrid>
-    </div>
-  );
+// GameGrid组件接受一个selectedGenre属性，用于过滤游戏列表
+const GameGrid = ({gameQuery}: Props) => {
+    const {data, error, isLoading} = useGame(gameQuery);
+    const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    return (
+        <div>
+            {error && <p>{error}</p>}
+            <SimpleGrid
+                columns={{sm: 1, md: 2, lg: 3, xl: 4}}
+                padding="10"
+                spacing={6}
+            >
+                {isLoading &&
+                    skeletons.map((s) => (
+                        <GameCardContainer key={s}>
+                            <GameCardSkeleton/>
+                        </GameCardContainer>
+                    ))}
+                {data.map((game) => (
+                    <GameCardContainer key={game.id}>
+                        <GameCard game={game}/>
+                    </GameCardContainer>
+                ))}
+            </SimpleGrid>
+        </div>
+    );
 };
 
 export default GameGrid;
