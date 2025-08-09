79358605c94b1a7688a25bf1e16b8123b195c389
陈海连 <948722262@qq.com>
2025-08-09 21:15:59 +0800

    处理错误路由

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage";
 import GameDetailPage from "./pages/GameDetailPage";
 import HomePage from "./pages/HomePage";
 import Layout from "./pages/Layout";
@@ -7,6 +8,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />, //父路由
+    errorElement:<ErrorPage/>,//错误路由
     children: [
       { index: true, element: <HomePage /> }, //默认子路由
       { path: "game/:id", element: <GameDetailPage /> }, //game子路由
